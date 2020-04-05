1273985103a5caf9ec53aae8dd0e6cda5e90ad9d
Lucas Werner <lucaswerner26@gmail.com>
2020-04-05 01:52:17 -0300

    first commit

---
+++ src/test/java/com/mycard/auth/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycard.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
